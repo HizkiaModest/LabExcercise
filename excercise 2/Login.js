04bfc178862238986b7ba5a495be8ba1325912e7
Hizkia Modest Eben Haezer <143594936+HizkiaModest@users.noreply.github.com>
2023-10-23 15:53:56 +0800

    Add files via upload

---
+++ excercise 2/Login.js
@@ -0,0 +1,16 @@
+// create button login
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
